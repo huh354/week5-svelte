2d2162f2e1c88d1b5fb16b1dbac9a27aacf8fc02
huh354 <143356635+huh354@users.noreply.github.com>
2023-09-24 23:32:03 -0400

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+
+<main>
+	<div class = "wrapper">
+<div class = "card">
+<div class = "header">
+
+  <h1>Welcome to my card</h1>
+  </div>
+
+  <img src="https://cdn.creatureandcoagency.com/uploads/2021/03/FrogLifecycle_Body_8.png">
+ 
+  <div class = "paragraph">
+  <p>This is a picture of a frog that I fond on the internet. It looks really cool and I thought it would be nice to share.</p>
+  </div>
+
+<div class="btn-wrapper">
+ <a href="https://hax.psu.edu">
+   <button>Details</button> </a> </div>
+  </div>
+  </div>
+
+
+</main>
+
